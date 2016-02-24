5fdb0c8256a6079c4aa22b503cc26adc0712e032
Adrián Domínguez Jiménez <adrian731gg@gmail.com>
2016-02-24 12:25:40 +0100

    Added files via upload

--- Ejercicio3xml.py
+++ Ejercicio3xml.py
@@ -7,12 +7,16 @@ accidents=etree.parse('accidentes.xml')
 raiz=accidents.getroot()
 accidentes=raiz.findall("result/accidente")
 
-
+teclado=int(raw_input("Introduce la edad máxima de un implicado en un accidente: "
 for a in accidentes:
-	if int(a.find("afectado/afectado/age").text) <= 30:
-		print a.find("afectado/afectado/age").text
-		print a.find("afectado/afectado/type").text
-		print a.find("afectado/afectado/status").text
+	
+	if int(a.find("afectado/afectado/age").text) <= teclado:
+		print a.find("afectado/afectado/age").text +"\n"+ a.find("afectado/afectado/type").text +"\n"+a.find("afectado/afectado/status").text
+		
+	else:
+		print "No hay accidentes con personas menores de: " +teclado+
+		teclado=int(raw_input("Introduce otra edad máxima: "
+		
 	
 		
 	
