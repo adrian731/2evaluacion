9dc42679c7902dcd626abe998aa10d67b92b9bf3
Adrián Domínguez Jiménez <adrian731gg@gmail.com>
2016-03-07 09:51:06 +0100

    Added files via upload

--- Ejercicio2xml.py
+++ Ejercicio2xml.py
@@ -9,10 +9,14 @@ raiz=accidents.getroot()
 accidentes=raiz.findall("result/accidente")
 
 teclado=raw_input( "Estamos tratando sobre accidentes. ¿Quiere buscar los que se han producido por un fallo mecánico o los que no?\nsi/no: ")
-for a in accidentes:
-	if teclado == a.find("falloMecanico"):
+teclado=teclado.upper()
+encontrado=False
+for a in accidentes :
+	if teclado == a.findtext("falloMecanico"):
+		encontrado=True
 		print a.find("vehiculo/vehiculo/quantity").text +" Vehículo/s implicados." 
 		print "Pavimento: "+ a.find("estadoPavimento").text
 		print "Atmófera: "+ a.find("estadoAtmosfera").text +"\n"
-	else:	
-		print "Error"
+if encontrado==False:
+	print "Ninguno encontrado"
+		
