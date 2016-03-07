5eee184cd97cb6bf59e4a4cf5b6ebd9aa07b24a7
Adrián Domínguez Jiménez <adrian731gg@gmail.com>
2016-03-07 14:37:17 +0100

    Added files via upload

--- Ejerciciojson4.py
+++ Ejerciciojson4.py
@@ -4,11 +4,11 @@ import json
 trabajo=open("trabajo.json","r")    
 trabajos=json.load(trabajo)
 
-teclado=raw_input("Introduce una cadena a buscar: ")
+teclado=raw_input("Introduce una fecha(YYYY-MM-DD): ")
 
 for t in trabajos["document"]["list"]:
 	for l in t["element"]["attribute"]:
-		if l["name"]=="Titulo_es":
-			if l["text"].count(teclado)>=1:
-				print l["text"]
-			
+		if l["name"]=="FechaModificacion":
+			if l["valor"].split(" ")[0] == teclado:
+				if l["name"]=="Enlace al contenido":
+					print l["valor"]
